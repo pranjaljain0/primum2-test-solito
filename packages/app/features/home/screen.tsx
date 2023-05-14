c8dda239f1fcb33e794617ab194422e0b17ce611
pranjaljain0 <pranjaljain0697@gmail.com>
2023-05-14 07:02:21 -0400

    chore: Fixing package-lock

--- packages/app/features/home/screen.tsx
+++ packages/app/features/home/screen.tsx
@@ -7,8 +7,7 @@ import Slide3 from 'app/components/slides/slide3';
 import Slide4 from 'app/components/slides/slide4';
 import Slide5 from 'app/components/slides/slide5';
 import SlideButton from 'app/components/Button/SlideButton';
-import { Text } from 'app/design/typography'
-import { UserSchema } from 'app/schema/index';
+import { UserSchema } from 'app/schema';
 import { View } from 'app/design/view'
 import image from "../../images/bg-sidebar-mobile.png"
 import { sidenav_data } from 'app/data/sidenav'
